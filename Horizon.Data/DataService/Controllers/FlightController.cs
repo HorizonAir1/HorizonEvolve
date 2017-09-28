a44cd7118dc2609fa46087757627f5129ca7d7d3
james kim <kim.jamesw@gmail.com>
2017-09-27 22:24:43 -0400

    added controllers

---
+++ Horizon.Data/DataService/Controllers/FlightController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataService.Controllers
+{
+    public class FlightController : ApiController
+    {
+        // GET: api/Flight
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Flight/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Flight
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Flight/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Flight/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
