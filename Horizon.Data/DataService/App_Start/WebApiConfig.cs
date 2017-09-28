4525ca916352be31a9b4ab478276ad7775cb13fc
james kim <kim.jamesw@gmail.com>
2017-09-27 22:20:02 -0400

    changed routing

--- Horizon.Data/DataService/App_Start/WebApiConfig.cs
+++ Horizon.Data/DataService/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace DataService
 
       config.Routes.MapHttpRoute(
           name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
+          routeTemplate: "DataService/{controller}/{id}",
           defaults: new { id = RouteParameter.Optional }
       );
     }
