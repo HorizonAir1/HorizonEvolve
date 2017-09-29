e98dcecf7de86ccd0893057c58271f35570c351c
james kim <kim.jamesw@gmail.com>
2017-09-29 18:10:33 -0400

    added models for new and returning passenger

---
+++ Horizon.Logic/LogicService/Models/NewPassenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicService.Models
+{
+  public class NewPassenger
+  {
+    public string Email { get; set; }
+    public string Firstname { get; set; }
+    public string Middle { get; set; }
+    public string Lastname { get; set; }
+    public System.DateTime Birth_date { get; set; }
+    public string Address { get; set; }
+    public string Tel_num { get; set; }
+  }
+}
---
+++ Horizon.Logic/LogicService/Models/ReturningPassenger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicService.Models
+{
+  public class ReturningPassenger
+  {
+    public string Email { get; set; }
+  }
+}
