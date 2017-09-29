b65fef191d3cd3e538d2ea54003720f83de4d094
james kim <kim.jamesw@gmail.com>
2017-09-29 12:15:42 -0400

    copied ls account into ds account

--- Horizon.Data/DataService/Controllers/AccountController.cs
+++ Horizon.Data/DataService/Controllers/AccountController.cs
@@ -9,31 +9,54 @@ namespace DataService.Controllers
 {
     public class AccountController : ApiController
     {
-        // GET: api/Account
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+    private static Dictionary<string, ClaimsIdentity> _UserStore = new Dictionary<string, ClaimsIdentity>();
 
-        // GET: api/Account/5
-        public string Get(int id)
+    public AccountController() : base()
+    {
+      if (!_UserStore.ContainsKey(ConfigurationManager.AppSettings["user"]))
+      {
+        var claims = new List<Claim>()
         {
-            return "value";
-        }
-
-        // POST: api/Account
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Account/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Account/5
-        public void Delete(int id)
-        {
-        }
+          new Claim(ClaimTypes.Role, "Logic", ClaimValueTypes.String),
+          new Claim("username", ConfigurationManager.AppSettings["user"]),
+          new Claim("password", ConfigurationManager.AppSettings["pass"])
+        };
+        var id = new ClaimsIdentity(claims, "ApplicationCookie");
+        _UserStore.Add(ConfigurationManager.AppSettings["user"], id);
+      }
     }
+
+    // GET: api/Account
+    public HttpResponseMessage Get()
+    {
+      return Request.CreateResponse<string>(HttpStatusCode.Forbidden, "No access");
+    }
+
+    //// GET: api/Account/5
+    //public string Get(int id)
+    //{
+    //  return "value";
+    //}
+
+    // POST: api/Account
+    public HttpResponseMessage Post(UserProfile p)
+    {
+      if (ModelState.IsValid && _UserStore.ContainsKey(p.Username))
+      {
+        var auth = Request.GetOwinContext().Authentication;
+        var props = new AuthenticationProperties()
+        {
+          IsPersistent = true,
+          ExpiresUtc = DateTime.UtcNow.AddHours(1)
+        };
+
+        auth.SignIn(props, _UserStore[p.Username]);
+        return Request.CreateResponse<string>(HttpStatusCode.Accepted, "Signed In");
+      }
+
+
+      return Request.CreateResponse<string>(HttpStatusCode.Forbidden, "No access");
+    }
+
+  }
 }
