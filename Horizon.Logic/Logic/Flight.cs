6b4a9fbd26d9f54e687866c611d8909be632d6ef
LeonMoore <leon.moore90@gmail.com>
2017-09-28 20:46:16 -0400

    initial push

--- Horizon.Logic/Logic/Flight.cs
+++ Horizon.Logic/Logic/Flight.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logic.Models
+namespace Logic
 {
     public class Flight
     {
+        public static bool GetAllSeats(int seats, int )
+        {
+
+        }
     }
 }
