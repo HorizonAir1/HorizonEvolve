5b1d160bf7568db5bae30f6428a58494e6d9cf67
james kim <kim.jamesw@gmail.com>
2017-09-28 20:57:16 -0400

    commented out stuff

--- Horizon.Data/DataService/App_Start/Startup.Auth.cs
+++ Horizon.Data/DataService/App_Start/Startup.Auth.cs
@@ -13,57 +13,67 @@ using DataService.Models;
 
 namespace DataService
 {
-    public partial class Startup
+  public partial class Startup
+  {
+    public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+    public static string PublicClientId { get; private set; }
+
+    // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+    public void ConfigureAuth(IAppBuilder app)
     {
-        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+      //// Configure the db context and user manager to use a single instance per request
+      //app.CreatePerOwinContext(ApplicationDbContext.Create);
+      //app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
-        public static string PublicClientId { get; private set; }
+      //// Enable the application to use a cookie to store information for the signed in user
+      //// and to use a cookie to temporarily store information about a user logging in with a third party login provider
+      //app.UseCookieAuthentication(new CookieAuthenticationOptions());
+      //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
-        public void ConfigureAuth(IAppBuilder app)
-        {
-            // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+      //// Configure the application for OAuth based flow
+      //PublicClientId = "self";
+      //OAuthOptions = new OAuthAuthorizationServerOptions
+      //{
+      //    TokenEndpointPath = new PathString("/Token"),
+      //    Provider = new ApplicationOAuthProvider(PublicClientId),
+      //    AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+      //    AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+      //    // In production mode set AllowInsecureHttp = false
+      //    AllowInsecureHttp = true
+      //};
 
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseCookieAuthentication(new CookieAuthenticationOptions());
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+      //// Enable the application to use bearer tokens to authenticate users
+      //app.UseOAuthBearerTokens(OAuthOptions);
 
-            // Configure the application for OAuth based flow
-            PublicClientId = "self";
-            OAuthOptions = new OAuthAuthorizationServerOptions
-            {
-                TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                // In production mode set AllowInsecureHttp = false
-                AllowInsecureHttp = true
-            };
+      var opt = new CookieAuthenticationOptions()
+      {
+        AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+        CookieHttpOnly = true,
+        CookieName = "logic-token",
+        LoginPath = new PathString("/DataService/Account/login")
+      };
 
-            // Enable the application to use bearer tokens to authenticate users
-            app.UseOAuthBearerTokens(OAuthOptions);
+      app.UseCookieAuthentication(opt);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+      // Uncomment the following lines to enable logging in with third party login providers
+      //app.UseMicrosoftAccountAuthentication(
+      //    clientId: "",
+      //    clientSecret: "");
 
-            //app.UseTwitterAuthentication(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+      //app.UseTwitterAuthentication(
+      //    consumerKey: "",
+      //    consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //    appId: "",
-            //    appSecret: "");
+      //app.UseFacebookAuthentication(
+      //    appId: "",
+      //    appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
-        }
+      //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+      //{
+      //    ClientId = "",
+      //    ClientSecret = ""
+      //});
     }
+  }
 }
