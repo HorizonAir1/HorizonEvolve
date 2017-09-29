a9de3e878175dc916627971bafa39c4058815207
LeonMoore <leon.moore90@gmail.com>
2017-09-29 14:02:20 -0400

    Adding functionality

---
+++ Horizon.Logic/Logic/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int PassengerId { get; set; }
+        public int FlightId { get; set; }
+        public int SeatClassId { get; set; }
+        public int SeatNumber { get; set; }
+        public int BaggageCount { get; set; }
+        public int BookingStatusId { get; set; }
+    }
+}
---
+++ Horizon.Logic/Logic/Models/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public TimeSpan ArrivalTime { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public TimeSpan DepartTime { get; set; }
+        public DateTime DepartDate { get; set; }
+        public string Destination { get; set; }
+        public string Departure { get; set; }
+        public int AircraftId { get; set; }
+    }
+}
