0dc43fa3d7ba1dba7ed7194b38715ebe86479898
james kim <kim.jamesw@gmail.com>
2017-09-29 12:19:34 -0400

    added packages

--- Horizon.Data/DataService/Controllers/AccountController.cs
+++ Horizon.Data/DataService/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace DataService.Controllers
