ab2993ac3b5d2622dc1713dd616280db4d530809
jaimejgonzalez <jaimejgonzalez1@gmail.com>
2017-10-02 12:46:03 -0400

    dbv2.4.3

--- Horizon.Data/DatabaseAPI/FacadeHelper.cs
+++ Horizon.Data/DatabaseAPI/FacadeHelper.cs
@@ -77,6 +77,11 @@ namespace DatabaseAPI
         {
             return lf.UpdateFlight(flight.flight_id,flight.arrival_time, flight.arrival_date, flight.dep_time, flight.dep_date, flight.destination, flight.departure, flight.aircraft_id);
         }
+        public bool DeleteFlight(FlightModel flight)
+        {
+            return lf.DeleteFlight(flight.flight_id);
+        }
+       
         public bool BookPassenger(BookingModel book)
         {
             return lf.BookPassenger(book.passenger_email, book.flight_id, book.seatclass_id,book.seat_number, book.baggage_num);
