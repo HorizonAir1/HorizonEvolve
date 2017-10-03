becde9113b040220d220e79c22e5a9d5113efca1
james kim <kim.jamesw@gmail.com>
2017-10-03 13:32:01 -0400

    changed helper to handler

--- Horizon.Logic/LogicService/Controllers/Handler/DataAPIHandler.cs
+++ Horizon.Logic/LogicService/Controllers/Handler/DataAPIHandler.cs
@@ -8,8 +8,18 @@ using System.Web;
 
 namespace LogicService.Controllers.Helpers
 {
-  public class DataAccountHelper
+  public class DataAPIHandler
   {
+    private WebRequestHandler _handler;
+
+    private static DataAccountHelper _instance;
+
+    private DataAccountHelper()
+    {
+      _handler
+    }
+
+
     public static bool Login()
     {
       var client = new HttpClient();
@@ -29,5 +39,18 @@ namespace LogicService.Controllers.Helpers
 
       return res.IsSuccessStatusCode;
     }
+
+    public static HttpResponseMessage GetResponse(string controllerString)
+    {
+      WebRequestHandler handler = new WebRequestHandler();
+      handler.CookieContainer = new System.Net.CookieContainer();
+      handler.UseCookies = true;
+      handler.UseDefaultCredentials = true;
+
+      HttpClient client = new HttpClient(handler);
+      client.BaseAddress = new Uri(ConfigurationManager.AppSettings["DataUri"]);
+      return response = client.GetAsync(controllerString).Result;
+    }
+    
   }
 }
