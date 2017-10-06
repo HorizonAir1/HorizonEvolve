0a9fd45b0659fdc7eed3408f156e9bdf13fe798e
james kim <kim.jamesw@gmail.com>
2017-10-06 11:04:34 -0400

    added startup.cs

---
+++ Horizon.Data/DatabaseAPI/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataService
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      var opt = new CookieAuthenticationOptions()
+      {
+        AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+        CookieHttpOnly = false,
+        CookieName = "Data-token",
+        LoginPath = new PathString("/DataService/Account")
+      };
+
+      app.UseCookieAuthentication(opt);
+    }
+  }
+}
