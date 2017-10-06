4bc1c8b97e05097d042f963a46b72ed640542bc3
LeonMoore <leon.moore90@gmail.com>
2017-10-06 01:25:12 -0400

    added CreateBooking(), CheckIfRepoExist(), GetBooking(), GetAllFlights(), UpdateFlight() and RemoveFlight()

--- Horizon.Logic/Logic/Repos/Repos.cs
+++ Horizon.Logic/Logic/Repos/Repos.cs
@@ -9,271 +9,357 @@ using System.Threading.Tasks;
 
 namespace Logic.Repos
 {
-  public class Repos
-  {
-    private List<Booking> _bookings;
-
-    private List<Passenger> _passengers;
-
-    private List<Flight> _flights;
-
-    private List<SeatingChart> _seats;
-
-    private List<Aircraft> _aircrafts;
-
-    private static Repos _instance;
-
-    private Repos(List<Passenger> passengers, List<Flight> flights, List<Booking> bookings)
+    public class Repos
     {
-      _passengers = passengers;
-      _flights = flights;
-      _bookings = bookings;
-    }
+        #region Repo Lists
+        private List<Booking> _bookings;
 
-    public static Repos Instance(Task<HttpResponseMessage> getPassengers, Task<HttpResponseMessage> getFlights, Task<HttpResponseMessage> getBookings)
-    {
-      if (_instance == null)
-      {
-        getFlights.Start();
-        getBookings.Start();
+        private List<Passenger> _passengers;
 
-        var passengers = getPassengers.GetAwaiter().GetResult();
-        List<Passenger> p;
-        if (passengers.IsSuccessStatusCode)
+        private List<Flight> _flights;
+
+        private List<SeatingChart> _seats;
+
+        private List<Aircraft> _aircrafts;
+
+        private static Repos _instance;
+
+        private Repos(List<Passenger> passengers, List<Flight> flights, List<Booking> bookings)
         {
-          var r = passengers.Content.ReadAsStringAsync().Result;
-          p = JsonConvert.DeserializeObject<List<Passenger>>(r);
+            _passengers = passengers;
+            _flights = flights;
+            _bookings = bookings;
         }
-        else return null;
 
-        getFlights.Wait();
-        var flights = getFlights.Result;
-        List<Flight> f;
-        if (flights.IsSuccessStatusCode)
+        public static Repos Instance(Task<HttpResponseMessage> getPassengers, Task<HttpResponseMessage> getFlights, Task<HttpResponseMessage> getBookings)
         {
-          var r = passengers.Content.ReadAsStringAsync().Result;
-          f = JsonConvert.DeserializeObject<List<Flight>>(r);
-        }
-        else return null;
+            if (_instance == null)
+            {
+                getFlights.Start();
+                getBookings.Start();
 
-        getBookings.Wait();
-        var bookings = getBookings.Result;
-        List<Booking> b;
-        if (passengers.IsSuccessStatusCode)
+                var passengers = getPassengers.GetAwaiter().GetResult();
+                List<Passenger> p;
+                if (passengers.IsSuccessStatusCode)
+                {
+                    var r = passengers.Content.ReadAsStringAsync().Result;
+                    p = JsonConvert.DeserializeObject<List<Passenger>>(r);
+                }
+                else return null;
+
+                getFlights.Wait();
+                var flights = getFlights.Result;
+                List<Flight> f;
+                if (flights.IsSuccessStatusCode)
+                {
+                    var r = passengers.Content.ReadAsStringAsync().Result;
+                    f = JsonConvert.DeserializeObject<List<Flight>>(r);
+                }
+                else return null;
+
+                getBookings.Wait();
+                var bookings = getBookings.Result;
+                List<Booking> b;
+                if (passengers.IsSuccessStatusCode)
+                {
+                    var r = passengers.Content.ReadAsStringAsync().Result;
+                    b = JsonConvert.DeserializeObject<List<Booking>>(r);
+                }
+                else return null;
+
+                _instance = new Repos(p, f, b);
+            }
+            return _instance;
+        }
+        #endregion
+
+        #region ClientCode
+        public List<Flight> GetAvailableFlightsWithDuration(string departLocation, string arrivalDestination, DateTime departureDate, DateTime arriveDate)
         {
-          var r = passengers.Content.ReadAsStringAsync().Result;
-          b = JsonConvert.DeserializeObject<List<Booking>>(r);
+            List<Flight> flights = new List<Flight>();
+            DateTime departDate = departureDate.Date;
+            DateTime arrivalDate = arriveDate.Date;
+            int numberOfdaysInFlight = (departDate - arrivalDate).Days;
+
+            foreach (Flight fli in _flights)
+            {
+                if (fli.Departure == departLocation && fli.Destination == arrivalDestination)
+                {
+                }
+            }
+
+            return null;
         }
-        else return null;
 
-        _instance = new Repos(p, f, b);
-      }
-      return _instance;
-    }
-
-    #region ClientCode
-    public List<Flight> GetAvailableFlightsWithDuration(string departLocation, string arrivalDestination, DateTime departureDate, DateTime arriveDate)
-    {
-      List<Flight> flights = new List<Flight>();
-      DateTime departDate = departureDate.Date;
-      DateTime arrivalDate = arriveDate.Date;
-      int numberOfdaysInFlight = (departDate - arrivalDate).Days;
-
-      foreach (Flight fli in _flights)
-      {
-        if (fli.Departure == departLocation && fli.Destination == arrivalDestination)
+        public Flight GetFlightDetails(int flightId)
         {
+            foreach (var fli in _flights)
+            {
+                if (fli.Id == flightId)
+                {
+                    return fli;
+                }
+            }
+
+            return null;
         }
-      }
 
-      return null;
-    }
-
-    public Flight GetFlightDetails(int flightId)
-    {
-      foreach (var fli in _flights)
-      {
-        if (fli.Id == flightId)
+        public void BookFlightWithSeat(int passengerId, int flightId, int seatId, int baggageCount, int seatClassId)
         {
-          return fli;
+            foreach (var fli in _bookings)
+            {
+                if (fli.PassengerId == passengerId && fli.FlightId == flightId && fli.SeatNumber == seatId && fli.BaggageNumber == baggageCount && fli.SeatClassId == seatClassId)
+                {
+                    _bookings.Add(new Booking()
+                    {
+                        PassengerId = passengerId,
+                        FlightId = flightId,
+                        SeatNumber = seatId,
+                        BaggageNumber = baggageCount,
+                        SeatClassId = seatClassId,
+                        StatusId = 4
+                    });
+                }
+            }
         }
-      }
 
-      return null;
-    }
-
-    public void BookFlightWithSeat(int passengerId, int flightId, int seatId, int baggageCount, int seatClassId)
-    {
-      foreach (var fli in _bookings)
-      {
-        if (fli.PassengerId == passengerId && fli.FlightId == flightId && fli.SeatNumber == seatId && fli.BaggageNumber == baggageCount && fli.SeatClassId == seatClassId)
+        public List<Booking> GetAllBookings()
         {
-          _bookings.Add(new Booking()
-          {
-            PassengerId = passengerId,
-            FlightId = flightId,
-            SeatNumber = seatId,
-            BaggageNumber = baggageCount,
-            SeatClassId = seatClassId,
-            StatusId = 4
-          });
+            return _bookings;
         }
-      }
+
+        public List<Flight> GetAllFlights()
+        {
+            return _flights;
+        }
+
+        public Booking GetBooking(int bookingId)
+        {
+            Booking booking = _bookings.FirstOrDefault(b => b.Id == bookingId);
+
+            if (booking != null)
+            {
+                return booking;
+            }
+
+            return null;
+        }
+
+        public IEnumerable<Booking> GetAllPassengerBookings(string email)
+        {
+            List<Booking> bookings = bookings = _bookings.Where(b => b.Passenger.Email == email).ToList();
+            return bookings;
+        }
+
+        public void CancelBooking(int bookingId)
+        {
+            Booking booking = _bookings.FirstOrDefault(b => b.Id == bookingId);
+
+            if (booking != null)
+            {
+                booking.StatusId = 5;
+            }
+        }
+
+        public void ChangeSeat(int bookingId, int newSeatId, int seatClassId)
+        {
+            Booking booking = _bookings.FirstOrDefault(b => b.Id == bookingId);
+
+            if (booking != null)
+            {
+                booking.SeatNumber = newSeatId;
+                booking.SeatClassId = seatClassId;
+            }
+        }
+
+        public IEnumerable<SeatingChart> GetAvailableSeat(int flightId)
+        {
+            List<SeatingChart> availableSeats = _seats.Where(s => s.FlightId == flightId && !s.IsTaken).ToList();
+
+            return availableSeats;
+        }
+
+        public Passenger GetPassenger(string email)
+        {
+            Passenger passenger = _passengers.FirstOrDefault(p => p.Email == email);
+
+            if (passenger != null)
+            {
+                return passenger;
+            }
+
+            return null;
+        }
+
+        public Flight GetFlight(int flightId)
+        {
+            Flight flight = _flights.FirstOrDefault(f => f.Id == flightId);
+
+            if (flight != null)
+            {
+                return flight;
+            }
+
+            return null;
+        }
+        #endregion
+
+        #region AdminCode
+        public void AddFlight<T>(Flight f, Task<T> task)
+        {
+            task.Start();
+            Flight flight = new Flight()
+            {
+                ArrivalTime = f.ArrivalTime,
+                ArrivalDate = f.ArrivalDate,
+                DepartTime = f.DepartTime,
+                DepartDate = f.DepartDate,
+                Destination = f.Destination,
+                Departure = f.Departure,
+                AircraftId = f.AircraftId
+            };
+
+            _flights.Add(flight);
+        }
+
+        public void CreatePassenger<T>(Passenger p, Task<T> task)
+        {
+            task.Start();
+            Passenger passenger = new Passenger()
+            {
+                FirstName = p.FirstName,
+                Middle = p.Middle,
+                LastName = p.LastName,
+                BirthDate = p.BirthDate,
+                Address = p.Address,
+                PhoneNumber = p.PhoneNumber,
+                Email = p.Email
+            };
+
+            _passengers.Add(passenger);
+        }
+
+        public void EditCustomerPersonalInfo<T>(Passenger update, Task<T> task)
+        {
+            task.Start();
+            Passenger passenger = _passengers.FirstOrDefault(p => p.Email == update.Email);
+
+            if (passenger != null)
+            {
+                passenger.FirstName = update.FirstName;
+                passenger.Middle = update.Middle;
+                passenger.LastName = update.LastName;
+                passenger.BirthDate = update.BirthDate;
+                passenger.Address = update.Address;
+                passenger.PhoneNumber = update.PhoneNumber;
+                passenger.Email = update.Email;
+            }
+        }
+
+        public void EditAircraftInfo(int aircraftId, string producer, int modelNumber, int seatActual, int seatMax)
+        {
+            Aircraft aircraft = _aircrafts.FirstOrDefault(a => a.Id == aircraftId);
+
+            if (aircraft != null)
+            {
+                aircraft.Producer = producer;
+                aircraft.ModelNumber = modelNumber;
+                aircraft.SeatActual = seatActual;
+                aircraft.SeatMax = seatMax;
+            }
+        }
+
+        public void UpdateFlight<T>(int flightId, Flight updateFlight, Task<T> task)
+        {
+            task.Start();
+            Flight flight = _flights.FirstOrDefault(f => f.FlightId == flightId);
+
+            if (flight != null)
+            {
+                flight.ArrivalTime = updateFlight.ArrivalTime;
+                flight.ArrivalDate = updateFlight.ArrivalDate;
+                flight.DepartTime = updateFlight.DepartTime;
+                flight.DepartDate = updateFlight.DepartDate;
+                flight.Destination = updateFlight.Destination;
+                flight.Departure = updateFlight.Departure;
+                flight.AircraftId = updateFlight.AircraftId;
+            }
+        }
+
+        public void RemoveClientFromFlight<T>(Booking book, Task<T> task)
+        {
+            task.Start();
+            Booking booking = _bookings.FirstOrDefault(b => b.FlightId == book.FlightId && b.PassengerId == book.Passenger.Id);
+
+            if (booking != null)
+            {
+                _bookings.Remove(booking);
+            }
+        }
+
+        public void RemoveFlight<T>(int flightId, Task<T> task)
+        {
+            task.Start();
+            Flight flight = _flights.FirstOrDefault(f => f.FlightId == flightId);
+
+            if (flight != null)
+            {
+                _flights.Remove(flight);
+            }
+
+        }
+
+        public void EditCustomerBooking<T>(Booking book, Task<T> task)
+        {
+            task.Start();
+            Booking booking = _bookings.FirstOrDefault(b => b.Id == book.Id);
+
+            if (booking != null)
+            {
+                booking.PassengerId = book.PassengerId;
+                booking.FlightId = book.FlightId;
+                booking.SeatClassId = book.SeatClassId;
+                booking.SeatNumber = book.SeatNumber;
+                booking.BaggageNumber = book.BaggageNumber;
+                booking.StatusId = book.StatusId;
+            }
+        }
+
+        public bool CheckIfPassengerExist(string email)
+        {
+            if (GetPassenger(email) == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CheckIfBookingExist(int passengerId, int flightId)
+        {
+            if (GetBooking(passengerId) == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void CreateBooking<T>(Booking book, Task<T> task)
+        {
+            task.Start();
+            Booking booking = new Booking()
+            {
+                PassengerId = book.PassengerId,
+                FlightId = book.FlightId,
+                SeatClassId = book.SeatClassId,
+                SeatNumber = book.SeatNumber,
+                BaggageNumber = book.BaggageNumber,
+                StatusId = book.StatusId
+            };
+
+            _bookings.Add(booking);
+        }
+        #endregion
     }
-
-    public List<Booking> GetAllBookings()
-    {
-      return _bookings;
-    }
-
-    public IEnumerable<Booking> GetAllPassengerBookings(string email)
-    {
-      List<Booking> bookings = bookings = _bookings.Where(b => b.Passenger.Email == email).ToList();
-      return bookings;
-    }
-
-    public void CancelBooking(int bookingId)
-    {
-      Booking booking = _bookings.FirstOrDefault(b => b.Id == bookingId);
-
-      if (booking != null)
-      {
-        booking.StatusId = 5;
-      }
-    }
-
-    public void ChangeSeat(int bookingId, int newSeatId, int seatClassId)
-    {
-      Booking booking = _bookings.FirstOrDefault(b => b.Id == bookingId);
-
-      if (booking != null)
-      {
-        booking.SeatNumber = newSeatId;
-        booking.SeatClassId = seatClassId;
-      }
-    }
-
-    public IEnumerable<SeatingChart> GetAvailableSeat(int flightId)
-    {
-      List<SeatingChart> availableSeats = _seats.Where(s => s.FlightId == flightId && !s.IsTaken).ToList();
-
-      return availableSeats;
-    }
-
-    public Passenger GetPassenger(string email)
-    {
-      Passenger passenger = _passengers.FirstOrDefault(p => p.Email == email);
-
-      if (passenger != null)
-      {
-        return passenger;
-      }
-
-      return null;
-    }
-    #endregion
-
-    #region AdminCode
-    public void AddFlight<T>(Flight f, Task<T> task)
-    {
-      task.Start();
-      Flight flight = new Flight()
-      {
-        ArrivalTime = f.ArrivalTime,
-        ArrivalDate = f.ArrivalDate,
-        DepartTime = f.DepartTime,
-        DepartDate = f.DepartDate,
-        Destination = f.Destination,
-        Departure = f.Departure,
-        AircraftId = f.AircraftId
-      };
-
-      _flights.Add(flight);
-    }
-
-    public void CreatePassenger<T>(Passenger p, Task<T> task)
-    {
-      task.Start();
-      Passenger passenger = new Passenger()
-      {
-        FirstName = p.FirstName,
-        Middle = p.Middle,
-        LastName = p.LastName,
-        BirthDate = p.BirthDate,
-        Address = p.Address,
-        PhoneNumber = p.PhoneNumber,
-        Email = p.Email
-      };
-
-      _passengers.Add(passenger);
-    }
-
-    public void EditCustomerPersonalInfo<T>(Passenger update, Task<T> task)
-    {
-      task.Start();
-      Passenger passenger = _passengers.FirstOrDefault(p => p.Email == update.Email);
-      
-      if (passenger != null)
-      {
-        passenger.FirstName = update.FirstName;
-        passenger.Middle = update.Middle;
-        passenger.LastName = update.LastName;
-        passenger.BirthDate = update.BirthDate;
-        passenger.Address = update.Address;
-        passenger.PhoneNumber = update.PhoneNumber;
-        passenger.Email = update.Email;
-      }
-    }
-
-    public void EditAircraftInfo(int aircraftId, string producer, int modelNumber, int seatActual, int seatMax)
-    {
-      Aircraft aircraft = _aircrafts.FirstOrDefault(a => a.Id == aircraftId);
-
-      if (aircraft != null)
-      {
-        aircraft.Producer = producer;
-        aircraft.ModelNumber = modelNumber;
-        aircraft.SeatActual = seatActual;
-        aircraft.SeatMax = seatMax;
-      }
-    }
-
-    public void RemoveClientFromFlight<T>(Booking book, Task<T> task)
-    {
-      task.Start();
-      Booking booking = _bookings.FirstOrDefault(b => b.FlightId == book.FlightId && b.PassengerId == book.Passenger.Id);
-
-      if (booking != null)
-      {
-        _bookings.Remove(booking);
-      }
-    }
-
-    public void EditCustomerBooking<T>(Booking book, Task<T> task)
-    {
-      task.Start();
-      Booking booking = _bookings.FirstOrDefault(b => b.Id== book.Id);
-
-      if (booking != null)
-      {
-        booking.PassengerId = book.PassengerId;
-        booking.FlightId = book.FlightId;
-        booking.SeatClassId = book.SeatClassId;
-        booking.SeatNumber = book.SeatNumber;
-        booking.BaggageNumber = book.BaggageNumber;
-        booking.StatusId = book.StatusId;
-      }
-    }
-
-    public bool CheckIfPassengerExist(string email)
-    {
-      if (GetPassenger(email) == null)
-      {
-        return false;
-      }
-
-      return true;
-    }
-    #endregion
-  }
 }
