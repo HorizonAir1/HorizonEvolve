2a07179f1747b783a71dccecfed582a3ec3dc835
LeonMoore <leon.moore90@gmail.com>
2017-10-08 19:08:27 -0400

    MVC Passenger Models

---
+++ Horizon.UI/MVC/Models/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Passenger
+    {
+        public int PassengerID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
