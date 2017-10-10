3a47730a7578f7fecf3ba6048b88f348a512290d
james kim <kim.jamesw@gmail.com>
2017-10-10 10:50:47 -0400

    added create flight

--- Horizon.UI/MVC/Controllers/FlightController.cs
+++ Horizon.UI/MVC/Controllers/FlightController.cs
@@ -1,4 +1,5 @@
-﻿using MVC.Models;
+﻿using MVC.Controllers.Handler;
+using MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,55 +8,60 @@ using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
-    public class FlightController : Controller
+  public class FlightController : Controller
+  {
+    LogicAPIHandler _lah = LogicAPIHandler.Instance;
+    // GET: Flight
+    public ActionResult Index()
     {
-        // GET: Flight
-        public ActionResult Index()
-        {
-            IEnumerable<Flight> flights = new List<Flight>();
+      IEnumerable<Flight> flights = new List<Flight>();
 
-            return View(flights);
-        }
-
-        // GET: Flight/Create
-        public ActionResult CreateFlight()
-        {
-            return View();
-        }
-
-        // POST: Flight/Create
-        [HttpPost]
-        public ActionResult CreateFlight(Flight flight)
-        {
-            //TODO: CreateFlight(flight);
-
-            return RedirectToAction("Index");
-        }
-
-        // GET: Flight/Edit/5
-        public ActionResult EditFlight(int id)
-        {
-            //TODO: GetFlight(id);
-
-            return View();
-        }
-
-        // POST: Flight/Edit/5
-        [HttpPost]
-        public ActionResult EditFlight(int flightId, Flight flight)
-        {
-            //Flight flightToUpdate = null; //TODO: GetFlight(id)
-                //TODO: Send to FlightAPI
-
-            return RedirectToAction("Index");
-        }
-
-        // GET: Flight/Delete/5
-        public ActionResult DeleteFlight(int flightId)
-        {
-            //TODO: DeleteFlight(flightId);
-
-            return View();
-        }
+      return View(flights);
     }
+
+    // GET: Flight/Create
+    public ActionResult CreateFlight()
+    {
+      return View();
+    }
+
+    // POST: Flight/Create
+    [HttpPost]
+    public ActionResult CreateFlight(Flight flight)
+    {
+      //TODO: CreateFlight(flight);
+      var res=_lah.PostResponse<Flight>("Flight/", flight);
+      if (res.IsSuccessStatusCode)
+      {
+        return RedirectToAction("Index");
+      }
+      return RedirectToAction("Index");
+    }
+
+    // GET: Flight/Edit/5
+    public ActionResult EditFlight(int id)
+    {
+      //TODO: GetFlight(id);
+
+      return View();
+    }
+
+    // POST: Flight/Edit/5
+    [HttpPost]
+    public ActionResult EditFlight(int flightId, Flight flight)
+    {
+      //Flight flightToUpdate = null; //TODO: GetFlight(id)
+      //TODO: Send to FlightAPI
+
+      return RedirectToAction("Index");
+    }
+
+    // GET: Flight/Delete/5
+    public ActionResult DeleteFlight(int flightId)
+    {
+      //TODO: DeleteFlight(flightId);
+
+      return View();
+    }
+  }
 }
