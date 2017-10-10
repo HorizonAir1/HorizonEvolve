3d693adffa8a842ba8dbeef3cb7f279b60e1148c
jaimejgonzalez <jaimejgonzalez1@gmail.com>
2017-10-10 19:17:17 -0400

    PUSHFunc

--- Horizon.UI_Angular/app/Controller/PaymentController.js
+++ Horizon.UI_Angular/app/Controller/PaymentController.js
@@ -1,45 +1,54 @@
-var PaymentController = function ($scope,$http,FlightService) {
-    $scope.clicked = function () {
-        window.location = "#/confirmation";
-    }
-    $scope.book = FlightService.getBooking();
+var PaymentController = function($scope, $http, FlightService) {
+  $scope.clicked = function() {
+    window.location = "#/confirmation";
+  }
+  $scope.book = FlightService.getBooking();
 
-    $scope.addtobook = function(){
+  $scope.addtobook = function() {
 
-        FlightService.addBooking($scope.fields.first);
-        FlightService.addBooking($scope.fields.last);
-        FlightService.addBooking($scope.fields.phone);
-        FlightService.addBooking($scope.fields.email);
+    FlightService.addBooking($scope.fields.first),
+      FlightService.addBooking($scope.fields.last),
+      FlightService.addBooking($scope.fields.phone),
+      FlightService.addBooking($scope.fields.email),
+      FlightService.addBooking($scope.fields.address),
+      FlightService.addBooking($scope.fields.date),
+      FlightService.addBooking($scope.fields.middle)
+  };
+
+  $scope.sendPost = function() {
+    var jbook ={
+        Email: $scope.fields.email,
+        Firstname: $scope.fields.first,
+        Middle: $scope.fields.middle,
+        Lastname: $scope.fields.last,
+        Birth_date: "09:00:00 00:00:00:0",
+        Address: $scope.fields.address,
+        Tel_num: $scope.fields.phone
     };
-
-    $scope.sendPost = function() {
-      var jbook = $.param({
-            json: JSON.stringify({
-                 passenger_email:"KIM@KIM.com",
-                 flight_id:1,
-                 seatclass_id:1,
-                 seat_number:20,
-                 baggage_num:2,
-                 status_id:4
-             })
-          });
-      console.log(jbook)
-      $http.post('http://ec2-18-221-5-183.us-east-2.compute.amazonaws.com/logic/logicservice/booking').success(function(jbook, status) {
-          console.log("WORKS");
-      }).error(function (data, status) {
-                    $scope.status = status;
-                });
+    var jsOne = JSON.stringify(jbook);
+    console.log(jsOne)
+    // $http.post('http://ec2-18-221-5-183.us-east-2.compute.amazonaws.com/logic/logicservice/passenger').success(function(jbook, status) {
+    //   console.log("WORKS");
+    // }).error(function(data, status) {
+    //   $scope.status = status;
+    // });
+    $http({
+        method: 'POST',
+        url: 'http://ec2-18-221-5-183.us-east-2.compute.amazonaws.com/logic/logicservice/passenger',
+        data: jsOne
+      })
+      .then(function successCallback(response) {});
   }
 
 
 
 }
 
-PaymentController.$inject = ['$scope','$http','FlightService'];
-//[booking_id] [int] IDENTITY(1,1) NOT NULL,
-	// [passenger_id] [int] NOT NULL,
-	// [flight_id] [int] NOT NULL,
-	// [seatclass_id] [int] NOT NULL,
-	// [seat_number] [int] NOT NULL,
-	// [baggage_num] [int] NOT NULL,
-	// [status_id] [int] NOT NULL,
+PaymentController.$inject = ['$scope', '$http', 'FlightService'];
+// string Email
+// string Firstname
+// string Middle
+// string Lastname
+// System.DateTime Birth_date
+// string Address
+// string Tel_num
