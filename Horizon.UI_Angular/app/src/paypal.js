3f79bf52c511ca4ccbf4b7c7826b4bc1b1048b8e
jaimejgonzalez <jaimejgonzalez1@gmail.com>
2017-10-10 11:17:31 -0400

    angularInclude

---
+++ Horizon.UI_Angular/app/src/paypal.js
@@ -0,0 +1,23 @@
+var button = document.querySelector('#submit-button');
+
+braintree.dropin.create({
+  authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+  selector: '#dropin-container'
+}, function (err, instance) {
+  if (err) {
+    // An error in the create call is likely due to
+    // incorrect configuration values or network issues
+    return;
+  }
+
+  button.addEventListener('click', function () {
+    instance.requestPaymentMethod(function (err, payload) {
+      if (err) {
+        // An appropriate error will be shown in the UI
+        return;
+      }
+
+      // Submit payload.nonce to your server
+    });
+  })
+});
